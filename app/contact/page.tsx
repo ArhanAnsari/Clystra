0d498d1b4dfd307e855360f81373796499ead8cb
unknown <arhanansari2009@gmail.com>
2025-04-13 19:36:27 +0530

    Fixed the Google Map Error

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -25,7 +25,9 @@ export default function ContactPage() {
             <p>Nagpur, Maharashtra</p>
           </div>
         </div>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3721.672163698438!2d79.10093887520027!3d21.12563248444443!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bd4c1004a0c6197%3A0xa1cb13640664c65a!2sClystra%20Networks%20Pvt.%20Ltd!5e0!3m2!1sen!2sin!4v1744552446612!5m2!1sen!2sin" width="800" height="600" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <div className="flex justify-center py-8">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3721.672163698438!2d79.10093887520027!3d21.12563248444443!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bd4c1004a0c6197%3A0xa1cb13640664c65a!2sClystra%20Networks%20Pvt.%20Ltd!5e0!3m2!1sen!2sin!4v1744552446612!5m2!1sen!2sin" width="800" height="600" style={{ border: 0 }} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+        </div>
       </section>
     </main>
   );
