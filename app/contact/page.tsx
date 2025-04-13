6d40993c8ed76078b3963644b0fbf8b7919c7d72
unknown <arhanansari2009@gmail.com>
2025-04-13 19:31:04 +0530

    Embeded the Google Map Location

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -25,6 +25,7 @@ export default function ContactPage() {
             <p>Nagpur, Maharashtra</p>
           </div>
         </div>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3721.672163698438!2d79.10093887520027!3d21.12563248444443!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bd4c1004a0c6197%3A0xa1cb13640664c65a!2sClystra%20Networks%20Pvt.%20Ltd!5e0!3m2!1sen!2sin!4v1744552446612!5m2!1sen!2sin" width="800" height="600" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
       </section>
     </main>
   );
